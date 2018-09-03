6a51f3e4b1a5c3b4283e6e0895b9788c10668851
Igor <igorcompuff@gmail.com>
2018-09-03 13:41:49 -0300

    Erased unused code

--- src/ddsa/test/ddsa-test-suite.cc
+++ src/ddsa/test/ddsa-test-suite.cc
@@ -1,7 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 // Include a header file from your module to test.
-#include "ns3/ddsa-routing-protocol-adapter.h"
 #include "ns3/ddsa-helper.h"
 #include "ns3/integer.h"
 #include "ns3/double.h"
